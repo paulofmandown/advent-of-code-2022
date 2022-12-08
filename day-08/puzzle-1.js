f42f7134fa49e6085a151657b3b68a2944cbc149
Paul Lewis <paul.lewis@cpsi.com>
2022-12-08 08:52:56 -0600

    Solved day 8 part 2

--- day-08/puzzle-1.js
+++ day-08/puzzle-1.js
@@ -15,12 +15,30 @@ const check = (grid, initialX, initialY, xMod, yMod) => {
   return false;
 };
 
+const getViewingDistance = (grid, initialX, initialY, xMod, yMod) => {
+  const treeHeight = grid[initialY][initialX];
+  let distance = 0;
+  for (
+    let [x, y] = [initialX + xMod, initialY + yMod];
+    grid[x] && grid[x][y];
+    [x, y] = [ x + xMod, y + yMod ]
+  ) {
+    distance += 1;
+    const obscuringHeight = grid[y][x];
+    if (treeHeight <= obscuringHeight) {
+      break;
+    };
+  }
+  return distance;
+};
+
 const solve = (err, data) => {
   const rows = data.toString().split("\n");
   rows.pop();
   const height = rows.length;
   const width = rows[0].length;
   let numExposed = height * 2 + width * 2 - 4;
+  let bestScenicScore = 0;
   console.log(`There are ${numExposed} trees around the edge`);
   for (let y = 1; y < height - 1; ++y) {
     for (let x = 1; x < width - 1; ++x) {
@@ -31,9 +49,17 @@ const solve = (err, data) => {
       if (!obscured) {
         numExposed += 1;
       }
+      const scenicScore = getViewingDistance(rows, x, y, -1, 0) *
+        getViewingDistance(rows, x, y, 1, 0) *
+        getViewingDistance(rows, x, y, 0, -1) *
+        getViewingDistance(rows, x, y, 0, 1);
+      if (scenicScore > bestScenicScore) {
+        bestScenicScore = scenicScore;
+      }
     }
   }
   console.log(`There are ${numExposed} exposed trees in total`);
+  console.log(`The best possible scenic score is ${bestScenicScore}`);
 };
 
 fs.readFile("day-08/input.txt", solve);
