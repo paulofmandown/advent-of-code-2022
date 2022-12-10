e5ab17deb43ebcb56ec3be7b965196dc6b327586
Paul Lewis <paul.lewis@cpsi.com>
2022-12-10 09:06:22 -0600

    encrypt input files; delete originals

---
+++ day-10/puzzle-1.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const solve = (err, data) => {};
+
+fs.readFile("day-10/input.txt", solve);
+// fs.readFile("day-10/example.txt", solve);
+// fs.readFile("day-10/example-2.txt", solve);
